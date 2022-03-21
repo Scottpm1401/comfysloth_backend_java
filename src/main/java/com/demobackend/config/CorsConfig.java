23f8a8846ee8e1f560220264d732166f66e1df09
Nhật Phan <scottpm1401@gmail.com>
2022-03-21 13:53:05 +0700

    deploy server

--- src/main/java/com/demobackend/config/CorsConfig.java
+++ src/main/java/com/demobackend/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.demobackend.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -9,13 +10,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @Configuration
 public class CorsConfig {
 
+    @Value("${frontend.url}")
+    private String frontendUrl;
     //Config CORS for cross-origin client
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurerAdapter() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("http://localhost:3000").allowedMethods("GET", "POST", "PUT", "PATCH","OPTIONS").allowedHeaders("Content-Type",
+                registry.addMapping("/**").allowedOrigins(frontendUrl).allowedMethods("GET", "POST", "PUT", "PATCH","OPTIONS").allowedHeaders("Content-Type",
                         "X-Requested-With",
                         "X-HTTP-Method-Override",
                         "access_token",
